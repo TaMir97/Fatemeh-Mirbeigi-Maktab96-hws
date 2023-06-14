73cf050decd0c288fa6524ff19aed3928be315a2
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-14 23:52:56 +0430

    create ProductRepo interface

---
+++ src/main/java/org/example/repository/ProductRepo.java
@@ -0,0 +1,16 @@
+package org.example.repository;
+
+import org.example.entity.Product;
+
+public interface ProductRepo {
+
+    void addNewProduct(Product product);
+
+    int updateProduct(Product product);
+
+    int deleteProduct(Product product);
+
+    Product isExist(String productName);
+
+    Product[] loadAllProducts();
+}

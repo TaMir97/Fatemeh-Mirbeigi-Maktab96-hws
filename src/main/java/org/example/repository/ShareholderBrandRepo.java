7e619eb5b921d799b3c41ee49dd39715e930c794
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-14 23:59:29 +0430

    create ShareholderBrandRepo interface

---
+++ src/main/java/org/example/repository/ShareholderBrandRepo.java
@@ -0,0 +1,12 @@
+package org.example.repository;
+
+import org.example.entity.ShareholderBrand;
+
+public interface ShareholderBrandRepo {
+    ShareholderBrand loadByShareholder(String shareholderName);
+    ShareholderBrand loadByBrand(String brandName);
+    ShareholderBrand[] loadAll();
+
+
+
+}

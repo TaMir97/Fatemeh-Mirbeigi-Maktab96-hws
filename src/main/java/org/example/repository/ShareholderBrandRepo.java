1a6898fd1fbb48baa66b90762d4717d26c4cd720
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-15 01:26:19 +0430

    modifications ShareholderBrandRepo interface

--- src/main/java/org/example/repository/ShareholderBrandRepo.java
+++ src/main/java/org/example/repository/ShareholderBrandRepo.java
@@ -1,10 +1,12 @@
 package org.example.repository;
 
+import org.example.entity.Brand;
+import org.example.entity.Shareholder;
 import org.example.entity.ShareholderBrand;
 
 public interface ShareholderBrandRepo {
-    ShareholderBrand[] loadByShareholder(String shareholderName);
-    ShareholderBrand[] loadByBrand(String brandName);
+    ShareholderBrand[] loadByShareholder(Shareholder shareholder);
+    ShareholderBrand[] loadByBrand(Brand brand);
     ShareholderBrand[] loadAll();
 
 

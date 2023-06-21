c69db8080d2ac7ce7d913d5e9ec5c0fa2edc3b6f
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-22 04:11:29 +0430

    modify extension

--- HW09/src/main/java/org/example/repository/ProductRepository.java
+++ HW09/src/main/java/org/example/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package org.example.repository;
 
+import org.example.base.repository.BaseRepository;
 import org.example.entity.Product;
 import org.example.exception.NotDefinedCategory;
 import org.example.exception.NotDefinedType;
@@ -7,9 +8,11 @@ import org.example.exception.NotDefinedType;
 import java.sql.SQLException;
 import java.util.List;
 
-public interface ProductRepository {
+public interface ProductRepository extends BaseRepository<Long, Product> {
     List<Product> findByCategory(String category) throws SQLException, NotDefinedCategory;
-    List<Product> findByType (String type) throws SQLException, NotDefinedType;
-    void updateAvailability(Product product, int chosenItems)throws SQLException;
+
+    List<Product> findByType(String type) throws SQLException, NotDefinedType;
+
+    void updateAvailability(Product product, int chosenItems) throws SQLException;
 
 }

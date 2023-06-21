1615d5521d89bf6c7036bc72a64921d236b5837c
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-21 23:15:44 +0430

    creating Cart entity

---
+++ HW09/src/main/java/org/example/entity/Cart.java
@@ -0,0 +1,15 @@
+package org.example.entity;
+
+import lombok.*;
+import org.example.base.entity.BaseEntity;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Cart extends BaseEntity<Long> {
+    private Long userID;
+    private int availability;
+    private long paycheck;
+}

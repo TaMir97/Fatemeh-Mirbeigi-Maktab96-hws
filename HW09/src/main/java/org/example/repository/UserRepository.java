62fa3f99401d0daab028e7653115acc28db3cf54
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-22 01:13:46 +0430

    define necessary and additional methods for user access

--- HW09/src/main/java/org/example/repository/UserRepository.java
+++ HW09/src/main/java/org/example/repository/UserRepository.java
@@ -2,11 +2,12 @@ package org.example.repository;
 
 import org.example.base.repository.BaseRepository;
 import org.example.entity.User;
+import org.example.exception.UsernameExistException;
 
 import java.sql.SQLException;
 
 public interface UserRepository extends BaseRepository<Long, User> {
-    boolean isExistsUsername(String username) throws SQLException;
+    boolean isExistsUsername(String username) throws SQLException, UsernameExistException;
 
     User findUserByUsername(String username) throws SQLException;
 

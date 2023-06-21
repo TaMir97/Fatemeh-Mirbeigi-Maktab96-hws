c3b3be2cfca64311a2b715dda9723c7b6760ba4d
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-21 23:20:00 +0430

    creating the many-to-many entity of cart-product

---
+++ HW09/src/main/java/org/example/entity/CartItems.java
@@ -0,0 +1,15 @@
+package org.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class CartItems {
+    private long cartID;
+    private long productID;
+}

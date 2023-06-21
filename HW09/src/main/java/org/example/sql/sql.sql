faf7799c73c8d0c8626d115a6143c38482ca326d
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-21 22:59:23 +0430

    creating tables of our entities

--- HW09/src/main/java/org/example/sql/sql.sql
+++ HW09/src/main/java/org/example/sql/sql.sql
@@ -3,11 +3,20 @@ create database online_shop;
 create table users
 (
     userID    bigserial primary key,
-    firstname varchar(50) not null,
-    lastname varchar(50) not null ,
+    firstname varchar(50)        not null,
+    lastname  varchar(50)        not null,
     user_name varchar(50) unique not null,
     password  varchar(50)        not null,
     email     varchar unique     not null
+);
+
+create table cart
+(
+    cartID      bigserial primary key,
+    userID      bigint,
+    foreign key (userID) references users (userID),
+    paycheck    bigint not null,
+    total_items int    not null
 );
 
 create table categories
@@ -15,18 +24,26 @@ create table categories
     category_name varchar unique not null
 );
 
-
-
-create table cart
+create table "types"
 (
-    cartID bigserial primary key,
-    userID bigint references users (userID),
-    paycheck bigint not null,
-    total_items int not null,
+    type_name varchar unique not null
+);
+
+create table products
+(
+    productID     bigserial primary key,
+    category_name varchar not null,
+    foreign key (category_name) references categories (category_name),
+    type_name     varchar not null,
+    foreign key (type_name) references "types" (type_name),
+    price         bigint  not null,
+    availability  int     not null
 );
 
 create table cart_items
 (
-    cartID     bigint references cart (cartID),
-    productID  bigint references products (productID)
+    cartID    bigint,
+    foreign key (cartID) references cart (cartID),
+    productID bigint,
+    foreign key (productID) references products (productID)
 );

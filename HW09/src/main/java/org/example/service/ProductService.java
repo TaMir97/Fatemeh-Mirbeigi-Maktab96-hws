e96c0b5dbdea5a98aa9ec1bf71d45f09e642e0c0
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-22 06:09:43 +0430

    ProductService interface defined

---
+++ HW09/src/main/java/org/example/service/ProductService.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import org.example.base.service.BaseService;
+import org.example.entity.Product;
+import org.example.exception.NotDefinedCategory;
+import org.example.exception.NotDefinedType;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ProductService extends BaseService<Long, Product> {
+    List<Product> findByCategory(String category) throws SQLException, NotDefinedCategory;
+    List<Product> findByType(String type) throws SQLException, NotDefinedType;
+    public void updateAvailabilityAdd(Product product, int chosenItems) throws SQLException;
+    void updateAvailabilityDelete(Product product, int chosenItems) throws SQLException;
+}

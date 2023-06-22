f6a59af5022b89cf7bd0e642a52ce5f9394bd8ca
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-22 07:37:23 +0430

    run!

--- HW09/src/main/java/org/example/App.java
+++ HW09/src/main/java/org/example/App.java
@@ -2,14 +2,9 @@ package org.example;
 
 import org.example.ui.Menu;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws Exception {
         Menu.run();
         
     }

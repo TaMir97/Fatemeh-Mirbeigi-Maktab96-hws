1bb6f9b781861c4a3c60e82f154eee7dbe3f1a34
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-26 23:27:51 +0430

    add new validation

--- HW10/src/main/java/org/example/validations/Validation.java
+++ HW10/src/main/java/org/example/validations/Validation.java
@@ -1,8 +1,12 @@
 package org.example.validations;
 
 public class Validation {
-    public static boolean inputValidation(String password) {
+    public static boolean inputLetterValidation(String password) {
         String regex = "^[a-z]$";
+        return password.matches(regex);
+    }
+    public static boolean inputIntegerValidation(String password) {
+        String regex = "^[0-9]+$";
         return password.matches(regex);
     }
 }

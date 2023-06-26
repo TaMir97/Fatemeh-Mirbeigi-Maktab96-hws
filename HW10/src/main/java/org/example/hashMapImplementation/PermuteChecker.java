76f064000fa3c38c3406efd7a6fbabc2a8704faf
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-26 19:23:30 +0430

    Check whether two inputs are permutations of each-other or not using hashMap and already existed class Permutation.java

---
+++ HW10/src/main/java/org/example/hashMapImplementation/PermuteChecker.java
@@ -0,0 +1,40 @@
+package org.example.hashMapImplementation;
+
+import java.util.*;
+
+public class PermuteChecker {
+    private final Scanner input;
+    private String firstInput;
+    private String secondInput;
+
+
+    public PermuteChecker() {
+        input = new Scanner(System.in);
+    }
+
+    private void getInputs() {
+        System.out.println("first input");
+        this.firstInput = input.nextLine();
+        System.out.println("second input");
+        this.secondInput = input.nextLine();
+        if (firstInput.length() <= 1 | secondInput.length() <= 1) {
+            throw new RuntimeException("The inputs ate not valid");
+        }
+    }
+
+    public void isPermutation() {
+        try {
+            getInputs();
+            List<String> allPermutations = Permutation.permutation(firstInput);
+            Map<String, List<String>> map = new HashMap<>();
+            map.put(firstInput, allPermutations);
+            if (map.get(firstInput).contains(secondInput)) {
+                System.out.println("passed");
+                return;
+            }
+            System.out.println("failed");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

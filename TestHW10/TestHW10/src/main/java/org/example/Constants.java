bdf57be5f679eea3dc45d77156a0643240d1bd63
Tahoora <75308106+TaMir97@users.noreply.github.com>
2023-06-29 06:39:19 +0330

    Delete TestHW10/TestHW10 directory

--- TestHW10/TestHW10/src/main/java/org/example/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Constants {
-    public static String[] RUN_ITEMS =
-            {"Get Permutations", "Check Permutation",
-                    "TreeSet Implementation", "Removing Improper Element",
-                    "Custom hashCMap", "Exit"};
-
-}

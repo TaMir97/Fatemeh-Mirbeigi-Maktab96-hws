ae7cc9da8fbcd6b77158c095385290edff49dcfc
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-06-29 07:52:43 +0430

    all!!!

---
+++ HW10Test/src/main/java/org/example/Constants.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Constants {
+    public static String[] RUN_ITEMS =
+            {"Get Permutations", "Check Permutation",
+                    "TreeSet Implementation", "Removing Improper Element",
+                    "Custom hashCMap", "Exit"};
+
+}
---
+++ HW10Test/src/main/java/org/example/Printer.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Printer {
+    public static void printMenu(String[] items) {
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(i + 1 + " " + items[i]);
+        }
+    }
+
+    public static void printMsg(String msg) {
+        System.out.println("--- " + msg + " ---");
+    }
+
+}

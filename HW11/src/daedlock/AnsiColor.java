112d733b06d9365f9b9e183b31e2be8f4ad0ee9a
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-06 00:08:27 +0430

    colouring the messages to clarify which is which! :)

---
+++ HW11/src/daedlock/AnsiColor.java
@@ -0,0 +1,10 @@
+package daedlock;
+
+public class AnsiColor {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+}

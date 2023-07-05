9545100e57dd3f11e2ad793dd20294f26f22391c
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-06 00:05:22 +0430

    call the read method to read the written messages

--- HW11/src/daedlock/Reader.java
+++ HW11/src/daedlock/Reader.java
@@ -3,5 +3,20 @@ package daedlock;
 import java.util.Random;
 
 public class Reader implements Runnable {
+    private final Message message;
 
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(new Random().nextInt(2000));
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
 }

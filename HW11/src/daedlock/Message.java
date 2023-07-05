de442ff252523424e66025fa19717ce16f6450e8
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-05 23:59:28 +0430

    add read and write methods that are synchronized but doesn't notify the other thread when the task is done

--- HW11/src/daedlock/Message.java
+++ HW11/src/daedlock/Message.java
@@ -1,6 +1,23 @@
 package daedlock;
 
 public class Message {
+    private String message;
+    private boolean empty = true;
 
+    public synchronized String read() {
+        while (empty) {
+
+        }
+        empty = true;
+        return message;
+    }
+
+    public synchronized void write(String message) {
+        while (!empty) {
+
+        }
+        empty = false;
+        this.message = message;
+    }
 
 }

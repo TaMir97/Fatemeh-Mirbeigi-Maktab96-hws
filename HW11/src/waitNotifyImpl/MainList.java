ca5354e68eaa7ff0651f08f00e8c246d6b3459ad
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-06 03:05:54 +0430

    slightly enhance the methods and flags again

--- HW11/src/waitNotifyImpl/MainList.java
+++ HW11/src/waitNotifyImpl/MainList.java
@@ -8,47 +8,40 @@ public class MainList {
     private int lastElement;
     private boolean evenFlag = true;
     private boolean oddFlag = false;
-    private boolean finished = false;
 
     public MainList(int lastElement) {
         mainList = new ArrayList<>();
         this.lastElement = lastElement;
     }
 
-    public synchronized void fillWithEven(int evenElement) throws InterruptedException {
-        while (!evenFlag || finished) {
-            wait();
+    public synchronized void fillWithEven(int evenElement) {
+        while (!evenFlag ) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
         }
         mainList.add(evenElement);
         evenFlag = false;
         oddFlag = true;
+
         notifyAll();
     }
 
-    public synchronized void fillWithOdd(int oddElement) throws InterruptedException {
-        while (!oddFlag || finished) {
-            wait();
+    public synchronized void fillWithOdd(int oddElement)  {
+        while (!oddFlag ) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
         }
         mainList.add(oddElement);
         oddFlag = false;
         evenFlag = true;
 
-        if (mainList.size() == lastElement + 1) {
-            finished = true;
-        }
-
         notifyAll();
     }
 
-    public void removeUnnecessary(int element) {
-        while (!finished) {
-            try {
-                wait();
-            } catch (InterruptedException ignored) {
-            }
-        }
-        mainList.remove(element);
-    }
 
     public int getLastElement() {
         return lastElement;

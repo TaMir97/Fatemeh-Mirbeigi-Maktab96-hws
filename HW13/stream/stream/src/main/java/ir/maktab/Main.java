d30f1df0ebee9f269ecb72c0b7c51f4e584dfabe
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-28 11:33:52 +0430

    complete the fourth question

--- HW13/stream/stream/src/main/java/ir/maktab/Main.java
+++ HW13/stream/stream/src/main/java/ir/maktab/Main.java
@@ -10,11 +10,21 @@ import java.util.stream.Collectors;
 public class Main {
   public static void main(String[] args) {
     List<Person> people = MockData.getPeople();
+    assert people != null;
 
+    //Q1
     List<Person> youngPeople = people.stream()
         .filter(person -> person.getAge() < 50)
         .toList();
     youngPeople.forEach(System.out::println);
+
+
+
+    //Q4
+    List<String> ipv4 = people.stream()
+        .map(Person::getIpv4)
+        .toList();
+    ipv4.forEach(System.out::println);
 
   }
 }

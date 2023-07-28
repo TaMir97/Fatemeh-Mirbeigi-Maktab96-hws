ac7b75c1dc3632bf2e357edd480b8af32aeaa3fd
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-28 18:59:06 +0430

    complete the fifth question for now(it needs justifications)

--- HW13/stream/stream/src/main/java/ir/maktab/Main.java
+++ HW13/stream/stream/src/main/java/ir/maktab/Main.java
@@ -6,6 +6,7 @@ import ir.maktab.model.Person;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,19 +28,35 @@ public class Main {
 //    sortedByUsername.forEach(System.out::println);
 
     //Q3
-    Comparator<Person> comparedByAgeThenLastName = Comparator
-        .comparing(Person::getAge)
-        .thenComparing(Person::getLastName);
-    List<Person> sortedByAgeThenLastName = people.stream()
-        .sorted(comparedByAgeThenLastName)
-        .toList();
-    sortedByAgeThenLastName.forEach(System.out::println);
+//    Comparator<Person> comparedByAgeThenLastName = Comparator
+//        .comparing(Person::getAge)
+//        .thenComparing(Person::getLastName);
+//    List<Person> sortedByAgeThenLastName = people.stream()
+//        .sorted(comparedByAgeThenLastName)
+//        .toList();
+//    sortedByAgeThenLastName.forEach(System.out::println);
 
     //Q4
 //    List<String> ipv4 = people.stream()
 //        .map(Person::getIpv4)
 //        .toList();
 //    ipv4.forEach(System.out::println);
+
+    //Q5
+    Map<String, Person> personMap = people.stream()
+        .sorted(Comparator.comparing(Person::getLastName))
+        .filter(person -> person.getGender().equalsIgnoreCase("Female") && person.getAge() > 40)
+        .dropWhile(person -> person.getFirstName().toLowerCase().startsWith("a"))
+        .skip(5)
+        .limit(100)
+        .collect(Collectors.toMap(
+            person -> person.getFirstName() + " " + person.getLastName(),
+            person -> person
+        ));
+    personMap.values().forEach(System.out::println);
+
+
+
 
   }
 }

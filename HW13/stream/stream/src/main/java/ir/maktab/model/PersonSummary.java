99ffb870ef388dc37ecd7790aeaf78058e72c385
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-07-28 20:10:48 +0430

    add the map method to the PersonSummary.java in order to get birthdate as date from json and assume the age correctly

--- HW13/stream/stream/src/main/java/ir/maktab/model/PersonSummary.java
+++ HW13/stream/stream/src/main/java/ir/maktab/model/PersonSummary.java
@@ -1,20 +1,41 @@
 package ir.maktab.model;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
+import java.text.SimpleDateFormat;
+import java.time.Year;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PersonSummary {
 
-    private Integer id;
-    private String firstName;
-    private String lastName;
-    private Integer age;
-    private Date birthDate;
+  private Integer id;
+  private String firstName;
+  private String lastName;
+  private Integer age;
+  private Date birthDate;
 
+  public static PersonSummary map(Person person) {
+
+    try {
+      SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+      Date birthDate = sdf.parse(person.getBirthDate());
+      int yearOfBirth = birthDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear();
+      int currentYear = Year.now().getValue();
+
+
+      return new PersonSummary(person.getId(), person.getFirstName(), person.getLastName(),
+          currentYear - yearOfBirth, birthDate);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
 }
+
+

1cf4933b4ae1905360bd9162f798d16a6f83f297
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-10 02:09:08 +0430

    change the column names

--- HW14/jpaHibernate/src/main/java/ir/maktab/domain/Teacher.java
+++ HW14/jpaHibernate/src/main/java/ir/maktab/domain/Teacher.java
@@ -4,14 +4,17 @@ import ir.maktab.base.domain.Person;
 import ir.maktab.domain.enumPack.AcademicRank;
 import ir.maktab.domain.enumPack.Degree;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
 public class Teacher extends Person {
+    @Column(name = "teacher_number")
     private Long teacherNo;
     private Degree degree;
+    @Column(name = "academic_rank")
     private AcademicRank academicRank;
     private BigDecimal payment;
 

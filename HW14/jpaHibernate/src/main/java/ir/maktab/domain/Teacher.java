492ac8bba4abb434574f3cf4c434af00d827752d
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-09 23:15:31 +0430

    complete Teacher entity

---
+++ HW14/jpaHibernate/src/main/java/ir/maktab/domain/Teacher.java
@@ -0,0 +1,71 @@
+package ir.maktab.domain;
+
+import ir.maktab.base.domain.Person;
+import ir.maktab.domain.enumPack.AcademicRank;
+import ir.maktab.domain.enumPack.Degree;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+public class Teacher extends Person {
+    private Long teacherNo;
+    private Degree degree;
+    private AcademicRank academicRank;
+    private BigDecimal payment;
+
+
+    public Teacher(Long personId, String firstname, String lastname, Date birthdate, Long teacherNo, Degree degree, AcademicRank academicRank, BigDecimal payment) {
+        super(personId, firstname, lastname, birthdate);
+        this.teacherNo = teacherNo;
+        this.degree = degree;
+        this.academicRank = academicRank;
+        this.payment = payment;
+    }
+
+    public Teacher() {
+    }
+
+    public Long getTeacherNo() {
+        return teacherNo;
+    }
+
+    public void setTeacherNo(Long teacherNo) {
+        this.teacherNo = teacherNo;
+    }
+
+    public Degree getDegree() {
+        return degree;
+    }
+
+    public void setDegree(Degree degree) {
+        this.degree = degree;
+    }
+
+    public AcademicRank getAcademicRank() {
+        return academicRank;
+    }
+
+    public void setAcademicRank(AcademicRank academicRank) {
+        this.academicRank = academicRank;
+    }
+
+    public BigDecimal getPayment() {
+        return payment;
+    }
+
+    public void setPayment(BigDecimal payment) {
+        this.payment = payment;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacherNo=" + teacherNo +
+                ", degree=" + degree +
+                ", academicRank=" + academicRank +
+                ", payment=" + payment +
+                '}';
+    }
+}

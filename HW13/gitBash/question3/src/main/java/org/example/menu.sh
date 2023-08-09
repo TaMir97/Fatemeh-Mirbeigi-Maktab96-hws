a5bf2f44dafa64193db63d910d7083ed63fec372
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-09 19:14:09 +0430

    the menu with bash script

--- HW13/gitBash/question3/src/main/java/org/example/menu.sh
+++ HW13/gitBash/question3/src/main/java/org/example/menu.sh
@@ -36,33 +36,40 @@ function replace_text_in_files() {
 
     echo "Text replaced successfully in files (file1.txt to file5.txt)."
 }
+function display() {
+    echo "Select an option:"
+            options=("Create 5 text files"
+                     "Add desired text to each"
+                     "Remove a word and replace it"
+                     "Quit")
+}
 
 function menu() {
-  echo "Select an option:"
-    options=("Create 5 text files"
-             "Add desired text to each"
-             "Remove a word and replace it"
-             "Quit")
 
     select choice in "${options[@]}"; do
+
         case "$choice" in
             "Create 5 text files")
                 echo "You chose Option 1: Create 5 text files."
                 create_five_txt_files
+                break
                 ;;
             "Add desired text to each")
                 echo "You chose Option 2: Add desired text to each."
                 add_text_to_files
+                break
                 ;;
             "Remove a word and replace it")
                 echo "You chose Option 3: Remove a word and replace it."
                 replace_text_in_files
+                break
                 ;;
             "Quit")
                 exit 0
                 ;;
             *)
                 echo "Invalid choice. Please enter a number from 1 to 4."
+                break
                 ;;
         esac
     done
@@ -70,5 +77,6 @@ function menu() {
 
 
 while true; do
+    display
     menu
 done

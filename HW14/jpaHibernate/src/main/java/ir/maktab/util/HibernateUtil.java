4370c7b58094bb6481ca4595378c22b3b0818819
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-10 06:47:54 +0430

    hibernate connection

---
+++ HW14/jpaHibernate/src/main/java/ir/maktab/util/HibernateUtil.java
@@ -0,0 +1,15 @@
+package ir.maktab.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+    private static final EntityManagerFactory emFactory;
+    static {
+        emFactory = Persistence.createEntityManagerFactory("default");
+    }
+    public static EntityManager getEntityManager(){
+        return emFactory.createEntityManager();
+    }
+}

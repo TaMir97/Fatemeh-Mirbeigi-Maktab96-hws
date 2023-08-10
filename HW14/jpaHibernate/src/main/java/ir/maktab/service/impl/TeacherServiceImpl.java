d9d946bd671a82ff6bcc5efd58ff4cbc16a09c1c
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-10 06:50:04 +0430

    save entity to the repository with customized signUp method

---
+++ HW14/jpaHibernate/src/main/java/ir/maktab/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,36 @@
+package ir.maktab.service.impl;
+
+import ir.maktab.base.service.PersonServiceImpl;
+import ir.maktab.domain.Teacher;
+import ir.maktab.domain.enumPack.AcademicRank;
+import ir.maktab.domain.enumPack.Degree;
+import ir.maktab.repository.TeacherRepo;
+import ir.maktab.repository.impl.TeacherRepoImpl;
+import ir.maktab.util.HibernateUtil;
+
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+import java.math.BigDecimal;
+
+public class TeacherServiceImpl extends PersonServiceImpl<Teacher>{
+    EntityManager entityManager = HibernateUtil.getEntityManager();
+    private final TeacherRepo teacherRepo = new TeacherRepoImpl(entityManager);
+
+
+    public Teacher signUpTeacher(String firstname, String lastname,
+                                 Long teacherNo, Degree degree, AcademicRank academicRank,
+                                 BigDecimal payment) {
+        Teacher teacher = new Teacher();
+        teacher.setFirstname(firstname);
+        teacher.setLastname(lastname);
+        teacher.setTeacherNo(teacherNo);
+        teacher.setDegree(degree);
+        teacher.setAcademicRank(academicRank);
+        teacher.setPayment(payment);
+        entityManager.clear();
+        teacherRepo.beginTransaction();
+        Teacher saved = teacherRepo.save(teacher);
+        teacherRepo.commitTransaction();
+        return saved;
+    }
+}

a1768e53e3bf6aab37684f1a525a1473c9d676a7
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-15 01:56:48 +0430

    Creating BaseEntity as the root of the generics

---
+++ HW15/src/main/java/org/example/base/domain/BaseEntity.java
@@ -0,0 +1,23 @@
+package org.example.base.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected ID id;
+}

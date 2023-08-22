11c97183acf85737d5911c971cdb98e759794aa8
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-22 13:44:00 +0430

    some modifications on salary bounds

--- HW15/src/main/java/org/example/domain/Employee.java
+++ HW15/src/main/java/org/example/domain/Employee.java
@@ -1,7 +1,26 @@
 package org.example.domain;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.example.domain.base.Person;
+import org.example.domain.enums.Role;
 
+
+@Getter
+@Setter
+@ToString
 public class Employee extends Person {
+    @Min(10_000_000)
     private Long salary;
+
+    public Employee() {
+    }
+
+    public Employee(String firstname, String lastname, String username, String password, String email, Role role, Long salary) {
+        super(firstname, lastname, username, password, email, role);
+        this.salary = salary;
+    }
 }

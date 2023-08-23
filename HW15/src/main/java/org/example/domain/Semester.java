e2449d4f35b1c8a302a845f4c6aedd729e2f517e
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-23 10:06:50 +0430

    complete the Semester.java

---
+++ HW15/src/main/java/org/example/domain/Semester.java
@@ -0,0 +1,36 @@
+package org.example.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.example.base.domain.BaseEntity;
+import org.example.domain.enums.Season;
+
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import java.time.Year;
+
+
+@Entity
+public class Semester extends BaseEntity<Long> {
+
+    private Season season;
+
+    private Integer year;
+
+    @PrePersist
+    private void prePersist() {
+        year = Year.now().getValue();
+    }
+
+    public void setSeason(Season season) {
+        this.season = season;
+    }
+
+    public Season getSeason() {
+        return season;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+}

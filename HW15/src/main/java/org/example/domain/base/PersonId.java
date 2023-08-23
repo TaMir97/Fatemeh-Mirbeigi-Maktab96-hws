91b1f3dfd0051103e4bb53841f7c28b48563b8e0
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-08-23 19:42:07 +0430

    make the class normal

--- HW15/src/main/java/org/example/domain/base/PersonId.java
+++ HW15/src/main/java/org/example/domain/base/PersonId.java
@@ -1,8 +1,8 @@
 package org.example.domain.base;
 
-//import javax.validation.constraints.NotBlank;
-//
-//import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+
+import javax.validation.constraints.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,29 +17,29 @@ import java.io.Serializable;
 @ToString
 @EqualsAndHashCode
 @Embeddable
-public abstract class PersonId implements Serializable {
+public class PersonId implements Serializable {
 
-//    @NotBlank(message = "First name is required")
-//    @Size(max = 100, message = "First name must not exceed 100 characters")
-//    @Pattern(regexp="^[a-zA-Z]+$", message = "FirstName must consist of letters only")
+    @NotBlank(message = "First name is required")
+    @Size(max = 100, message = "First name must not exceed 100 characters")
+    @Pattern(regexp="^[a-zA-Z]+$", message = "FirstName must consist of letters only")
     private String firstname;
 
-//    @NotBlank(message = "Last  name is required")
-//    @Size(max = 100, message = "First name must not exceed 100 characters")
-//    @Pattern(regexp="^[a-zA-Z]*$", message = "LastName must consist of letters only")
+    @NotBlank(message = "Last  name is required")
+    @Size(max = 100, message = "First name must not exceed 100 characters")
+    @Pattern(regexp="^[a-zA-Z]*$", message = "LastName must consist of letters only")
     private String lastname;
 
-//    @NotBlank(message = "Username is required")
-//    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
-//    @Pattern(regexp = "^[0-9]+$", message = "Username must consist of numbers only")
+    @NotBlank(message = "Username is required")
+    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
+    @Pattern(regexp = "^[0-9]+$", message = "Username must consist of numbers only")
     private String username;
 
-//    @NotBlank(message = "Password is required")
-//    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
-//    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password must consist of letters and numbers only")
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Username must be between 6 and 20 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Password must consist of letters and numbers only")
     private String password;
 
-//    @Email(message = "Email should be valid")
+    @Email(message = "Email should be valid")
     private String email;
 
 

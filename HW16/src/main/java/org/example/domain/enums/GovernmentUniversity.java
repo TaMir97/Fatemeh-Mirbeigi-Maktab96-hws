38b6a654b4297af70a87cb86afd70b5b47e77719
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-09-12 16:48:06 +0430

    required enums

---
+++ HW16/src/main/java/org/example/domain/enums/GovernmentUniversity.java
@@ -0,0 +1,6 @@
+package org.example.domain.enums;
+
+public enum GovernmentUniversity {
+    FUNDED,
+    NO_FUND
+}
---
+++ HW16/src/main/java/org/example/domain/enums/UniversityType.java
@@ -0,0 +1,10 @@
+package org.example.domain.enums;
+
+public enum UniversityType {
+    DOWLATI,
+    GHEIRE_ENTEFAEI,
+    PAYAME_NOOR,
+    ELMI_KARBORDI,
+    AZAD
+
+}

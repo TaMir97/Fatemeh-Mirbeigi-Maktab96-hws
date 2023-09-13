cc8e890023d6e4449401e0c3a39919cd119595d0
Tahoora Mirbeigi <tahoora_m@yahoo.com>
2023-09-13 11:41:35 +0430

    test whether the card information is valid or not

---
+++ HW16/src/test/java/org/example/util/CardInfoTest.java
@@ -0,0 +1,43 @@
+package org.example.util;
+
+import junit.framework.TestCase;
+import org.example.domain.CardInfo;
+
+
+public class CardInfoTest extends TestCase {
+
+    public void testValidCardInfo() {
+        String[] validValues = {
+                "5859831134567890",
+                "6073998123456789",
+                "5859831123456789",
+                "6280852123456789"
+        };
+
+        for (String value : validValues) {
+            try {
+                CardInfo cardInfo = new CardInfo(value);
+            } catch (IllegalArgumentException e) {
+                fail("IllegalArgumentException should not have been thrown for valid input: " + value);
+            }
+        }
+    }
+
+    public void testInvalidCardInfo() {
+        String[] invalidValues = {
+                "1234567890123456",
+                "58596312345",
+                "58596312345678901",
+                "abcdefg1234567890"
+        };
+
+        for (String value : invalidValues) {
+            try {
+                new CardInfo(value);
+                fail("Expected IllegalArgumentException was not thrown");
+            } catch (IllegalArgumentException e) {
+                // This is expected
+            }
+        }
+    }
+}
